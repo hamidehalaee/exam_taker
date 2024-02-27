476f31ae46218ce976dd5d687d4d5f57fc8e41f3
hamidehalaee <hamidehalaeeyasar@gmail.com>
2024-02-27 15:21:15 +0330

    update schemas

--- schemas.py
+++ schemas.py
@@ -1,60 +1,34 @@
 #models from Pydantic
 from pydantic import BaseModel, list
 
-
-
-from pydantic import BaseModel
-
-
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
 class UserBase(BaseModel):
     email: str
 
-
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: list[Item] = []
-
-    class Config:
-        orm_mode = True
-
-class User(BaseModel):
-    username: str
+class UserCreated(UserBase):
     password: str
 
 class Exam(BaseModel):
     id: int
     name: str
-    questions: List[str]
+    questions: list[str]
+
+class User(UserBase):
+    id: int
+    username: str
+    password: str
+    is_active: bool
+    items: list[Exam] = []
+
+    class Config:
+        orm_mode = True
 
 class Question(BaseModel):
     id: str
     text: str
-    options: List[str]
+    options: list[str]
 
 class Submission(BaseModel):
     username: str
     exam_id: int
-    answers: List[str]
+    answers: list[str]
 
